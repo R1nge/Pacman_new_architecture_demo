0c6899441229ac46a0ba5d485658663f311db6b7
R1nge <r1ngegamedev@gmail.com>
2025-10-14 23:07:54 +0300

    Entity tags

---
+++ Assets/_Assets/Scripts/Ecs/Tags/BlinkyTag.cs
@@ -0,0 +1,9 @@
+using Scellecs.Morpeh;
+
+namespace _Assets.Scripts.Ecs.Tags
+{
+    public struct BlinkyTag : IComponent
+    {
+        
+    }
+}
---
+++ Assets/_Assets/Scripts/Ecs/Tags/ClydeTag.cs
@@ -0,0 +1,8 @@
+using Scellecs.Morpeh;
+
+namespace _Assets.Scripts.Ecs.Tags
+{
+    public struct ClydeTag : IComponent
+    {
+    }
+}
