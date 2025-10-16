eb08b8191037d272de3fa1506a55e265c2ab4913
R1nge <r1ngegamedev@gmail.com>
2025-10-16 15:15:43 +0300

    Ball spawner remove magic number

--- Assets/_Assets/Scripts/Services/BallSpawner.cs
+++ Assets/_Assets/Scripts/Services/BallSpawner.cs
@@ -10,10 +10,15 @@ namespace _Assets.Scripts.Services
     {
         [Inject] private IObjectResolver _objectResolver;
         [Inject] private ConfigProvider _configProvider;
-        private GameObject[,] _balls = new GameObject[31, 28];
+        private GameObject[,] _balls;
 
         public GameObject Create(Vector3 position)
         {
+            if (_balls == null || _balls.GetLength(0) != _configProvider.GameConfig.Width || _balls.GetLength(1) != _configProvider.GameConfig.Height)
+            {
+                _balls = new GameObject[_configProvider.GameConfig.Width, _configProvider.GameConfig.Height];
+            }
+
             var ball = _objectResolver.Instantiate(_configProvider.GameConfig.Point, position, Quaternion.identity);
             _balls[(int)position.x, (int)position.y] = ball;
             return ball;
